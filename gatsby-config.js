7073f0295bf35f25496afd1cc9dcb959b5db04f3
Scott Montford <scottmontford@gmail.com>
2020-09-17 01:00:14 +0100

    Build 0.06
    -Temporary Favicon fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,6 @@ module.exports = {
                 background_color: '#f7f0eb',
                 theme_color: '#a2466c',
                 display: 'standalone',
-                icon: 'public/images/favicon.png',
             }
         }
     ]
