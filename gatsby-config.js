87447f625669404144adca0e028b31c81ffd0ca2
Scott Montford <scottmontford@gmail.com>
2020-09-17 01:21:24 +0100

    Build 0.071
    -NetlifyCMS logo test
    -Favicon test

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,7 @@ module.exports = {
                 background_color: '#f7f0eb',
                 theme_color: '#a2466c',
                 display: 'standalone',
+                logo_url: 'images/favicon.png',
             }
         }
     ]
