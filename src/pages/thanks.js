c4e2b1c8d78f4d0fd7678eb765577d33196887ca
Scott Montford <scottmontford@gmail.com>
2020-10-11 20:30:42 +0100

    Build 0.33
    -Fixed contact form

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default () => (
   <div>
-    <h1>Thank you for your submission!</h1>
+    <h1>Thank you for your submission!</h1><br>
+    </br>
+    <a href="/contact-us">Click here to return</a>
   </div>
 ) 
