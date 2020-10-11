80fcb6a5dbc893be582224bf2296cf1dc36692bc
Scott Montford <scottmontford@gmail.com>
2020-10-11 20:12:43 +0100

    Fixed contact????

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -19,27 +19,6 @@ export const query = graphql`
 
 export default class Page extends React.Component {
     render() {
-<<<<<<< Updated upstream
-=======
-      const handleChange = (e) => {
-        setState({ ...state, [e.target.name]: e.target.value })
-      }
-    
-      const handleSubmit = (e) => {
-        e.preventDefault()
-        const form = e.target
-        fetch('/', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: encode({
-            'form-name': form.getAttribute('name'),
-            ...state,
-          }),
-        })
-          .then(() => navigate(form.getAttribute('action')))
-          .catch((error) => alert(error))
-      }
->>>>>>> Stashed changes
 
         return (
             <Layout {...this.props}>
